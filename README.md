35a27f179ec09b0980fe49e7d9ee03d2fd1f008a
kuldeepMali <kmali@tekion.com>
2023-07-08 23:49:00 +0530

    imdb ratings for amazon prime

---
+++ README.md
@@ -0,0 +1,70 @@
+![done](https://user-images.githubusercontent.com/37064367/67810093-2a487080-fa57-11e9-87ec-7ce0832c501a.png)
+
+# Amazon Prime Video - Rotten Tomatoes Overlay
+Overlays Amazon Prime Video™ with Rotten Tomatoes™ movie and TV ratings.
+
+This extension's chrome store webpage can be found [here](https://chrome.google.com/webstore/detail/amazon-prime-video-rotten/hfemgdpfoemphmhakjpcbepaggjmhjci).
+
+This chrome extension provides scores from Rotten Tomatoes™ to the media included within the Amazon Prime Video™ service.
+
+Simply hover your mouse over a movie or TV show on the Amazon Prime Video™ website to use this extension. 
+If you find any issues using this extension, feel free to leave a message in the 'support' section.
+
+![imgFound](https://user-images.githubusercontent.com/37064367/67810619-2406c400-fa58-11e9-93d0-720b3f13600f.jpg)
+
+## How It Works
+At a high level, this extension uses the title of the movie/tv-show viewed by the user on prime video as input, sends that data to google s.e and takes the url of the first rotten-tomato web result found, and then searches rotten tomatoes for the correct audience rating/total scores, and then displays that back to the user. This all takes place client side; this extension does not collect user data. It is also completely free. Due to all the moving parts/dependence on external websites, this extension breaks frequently. If google, amazon prime, or rotten tomatoes has a page update, this extension will mostly likely need to, and be updated within 1-3 weeks.
+
+__Link to Chrome Store__
+* https://chrome.google.com/webstore/detail/amazon-prime-video-rotten/hfemgdpfoemphmhakjpcbepaggjmhjci
+
+## Change Log
+###### v1.61 ######
+- Add static slider, (non-video), support"
+###### v1.60 ######
+- Fix error caused by  google search element change (again)
+###### v1.59 ######
+- Fix errors caused by google search element change
+###### v1.58 ######
+- Fix multiple fatal errors caused by rotten tomato and amazom prime video site updates
+###### v1.53 ######
+- Fix fatal error caused by google search engine update
+###### v1.52 ######
+- Added domain support for known smile.amazon URL extensions
+###### v1.51 ######
+- Fix fatal error where extension attempts to access incorrect element of Prime Video element on sliders causing sliders not displaying ratings.
+###### v1.5 ######
+- Added support for checking sliders below single page of media. Refactored majority of extension for readability,
+efficiency, and ease of use.
+###### v1.43 ######
+- Fix fatal error where extension attempts to access incorrect element of alphabet inc search resulting in 'page not found' error
+###### v1.42 ######
+- Domain support added for users in the United Kingdom (www.amazon.co.uk/gp/video/)
+###### v1.41 ######
+- Update element names with new names
+###### v1.4 ######
+- Complete overhaul of extension was necessary after Prime Video UI experienced a complete change
+- Added support for signed in users of Prime Video, and those who are not signed in
+- In addition to displaying ratings on the Prime Video homepage, extension now displays ratings when a user clicks on film/video, and when hovers over 'Customers who watched this item also watched' 
+###### v1.37 ######
+- Domain support added for Australian users, and fixed for Brazilian users*, and all countries that use the URL (primevideo.com/*) Loading image resized to be less obtrusive 
+###### v1.33 ######
+- Domain support added for Brazilian users *post-release-fix of this update should have fixed any more issues for Brazilian users*
+###### v1.31 ######
+- Fix fatal error where extension attempts to access incorrect element of search resulting in 'page not found' error, and support for 'www.Amazon.de" added
+###### v1.29 ######
+- Fix fatal error where extension is unable to run due to recent update to google search where all visible URLS were replaced with breadcrumb styling
+###### v1.23 ######
+- Fix issue where extension does not run if prime video URL includes '/gp/' when the user accesses the different options on the 'Amazon Prime Video' page, ex: 'all videos' , 'your videos' , 'included with prime' , etc
+###### v1.22 ######
+- Fix issue where extension does not run if prime video URL includes '/Prime-Video/' instead of '/Amazon-Video/'
+###### v1.21 ######
+- Change extension name and description 
+###### v1.2 ######
+- Fix issue where extension does not run if prime video is accessed through the 'Your 
+  Prime Video' option under 'My account' 
+###### v1.1 ######
+- Change manifest with short name addition
+
+## Disclaimer
+Amazon™ , Amazon Prime Instant Video™ and Rotten Tomatoes™ are registered trademarks and have no affiliation to this extension or the author of said extension.
---
+++ contentscript.js
@@ -0,0 +1,229 @@
+var rtClassName = 'Rotten-Tomatoes';
+var rtClassNameSP = 'Rotten-Tomatoes-SP';
+
+// On page load listener, when page fully loaded, execute mouse listener
+window.addEventListener('load', (event) => {
+
+	// Mouse listener for any move event on the current document.
+	document.addEventListener('mousemove', function (e) {
+	let srcElement = e.srcElement;
+
+// ================================ Function Definitions ===============================
+
+
+// ------------------------------SLIDER CHECK----------------------------------------
+function slider(cursorElement) {
+	var sliderNoVideoOrJunk = '_3RLSRf';
+	var sliderWithVideo = 'tst-video-overlay-player-html5';
+	var sliderFound = 'lAtJLC';
+	
+	//console.log(cursorElement.className);
+
+	if (typeof(cursorElement) !== 'undefined'){
+		if(cursorElement.className == sliderWithVideo){
+			//console.log(cursorElement.parentElement.nextSibling.className);
+			if((cursorElement.parentElement.nextSibling.className == sliderFound) 
+			&& cursorElement.parentElement.nextSibling.lastChild.className != rtClassName){
+				return cursorElement.parentElement.nextSibling;
+			}
+		}
+		else if((cursorElement.className == sliderNoVideoOrJunk) && 
+		(cursorElement.parentElement.parentElement.parentElement.parentElement.nextSibling)){
+			//console.log(cursorElement.parentElement.parentElement.parentElement.parentElement.nextSibling);
+			if((cursorElement.parentElement.parentElement.parentElement.parentElement.nextSibling.className == sliderFound)
+			&& cursorElement.parentElement.parentElement.parentElement.parentElement.nextSibling.lastChild.className != rtClassName){
+				return cursorElement.parentElement.parentElement.parentElement.parentElement.nextSibling;
+			}
+		}
+		else{
+			return false;
+		}	
+	}
+	else{
+		return false;
+	}
+}
+
+// ------------------------------SLIDER ON SINGLE PAGE CHECK----------------------------------------
+function sliderOnSinglePage(cursorElement){
+	//console.log(cursorElement);
+	var sliderOnPage = 'Zk8aEm _1EcvsB _1Asy_o';
+	var sliderFound = 'lAtJLC';
+
+	if (typeof(cursorElement) !== 'undefined'){
+		if((cursorElement.className == sliderOnPage) && 
+		(cursorElement.parentElement.parentElement.parentElement.parentElement.nextSibling)){
+			if((cursorElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextSibling.className == sliderFound)
+			&& cursorElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextSibling.lastChild.className != rtClassName){
+				return cursorElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextSibling;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	else{
+		return false;
+	}
+}
+
+// ------------------------------SINGLE PAGE CHECK----------------------------------------
+function singlePage(){
+	if(document.getElementsByClassName("Rotten-Tomatoes-SP")[0] == null){
+		if(document.getElementsByClassName('av-detail-section')[0]){
+			return document.getElementsByClassName('_3QwtCH _16AW_S _2LF_6p dv-node-dp-badges _3vxMzE HaWow5')[0];
+		}
+		else
+			return false;
+	}
+}
+
+// --------------------APPEND ROTTEN TOMATOES SPAN/DIV - (LOADING) --------------------------
+function appendRottenTomatoes(AZparentClass,nameOfSpan){
+
+	//console.log(AZparentClass)
+	//console.log(nameOfSpan)
+
+	// -- Creates the RT span
+	var RTspan = document.createElement('span');
+	RTspan.className = nameOfSpan;
+	AZLastChild = AZparentClass.lastChild;
+
+	// -- Inserts the new rotten tomatoes class before the last child element in the amazon parent class
+	AZparentClass.insertBefore(RTspan,AZLastChild.nextSibling);
+						
+	// -- Creates a loading image
+	var loading = document.createElement("img");
+	loading.src = chrome.extension.getURL("images/loading.gif");
+	loading.className = "loading";
+
+	// -- Appends loading.gif to the RTspan
+	if(nameOfSpan == 'Rotten-Tomatoes'){
+		AZparentClass.getElementsByClassName("Rotten-Tomatoes")[0].appendChild(loading); 
+	}
+	else if(nameOfSpan == 'Rotten-Tomatoes-SP'){
+		AZparentClass.getElementsByClassName("Rotten-Tomatoes-SP")[0].appendChild(loading); 
+	}
+	return RTspan;
+}
+
+// --------------- SEARCHES FOR CORRECT SEARCH TERMS FOR SLIDER ELEMENT ---------------------
+function sliderGoogleUrl(sliderParentElement){
+	// Grabs the year the media was made
+	if(sliderParentElement.getElementsByClassName('nPUwP8 _2Z_R2D')[0]){
+		var titleYear = sliderParentElement.getElementsByClassName('nPUwP8 _2Z_R2D')[0].innerText;
+		if(titleYear.includes("min")){
+			titleYear = sliderParentElement.getElementsByClassName('nPUwP8 _2Z_R2D')[1].innerText;
+		}
+	}
+	//console.log(titleYear);
+
+	// Grabs the title of the movie / tvshow from the amazon video DOM
+	var titleOfMedia = sliderParentElement.getElementsByClassName('_2MiS8F tst-hover-title')[0].innerText;
+	//console.log(titleOfMedia)
+
+	// Formats and appends data to create google search
+	var formattedTitle = titleOfMedia.split(' ').join('%20');
+	url = 'https://google.com/search?q=Rotten%20Tomatoes%20' + formattedTitle + '%20' + '(' + titleYear + ')' + '%20';
+	//console.log(url)
+
+	return url;
+}
+
+// --------------- SEARCHES FOR CORRECT SEARCH TERMS FOR SINGLE PAGE ---------------------
+function singlePageGoogleUrl(){
+	// Grabs the year the media was made
+	var titleYear = document.querySelector('[data-automation-id="release-year-badge"]').innerText;
+	
+	// Grabs the title of the movie / tvshow from the amazon video DOM
+	var titleOfMedia = document.getElementsByClassName('_2IIDsE _3I-nQy')[0].innerText;
+	
+	// Formats and appends data to create google search
+	var formattedTitle = titleOfMedia.split(' ').join('%20');
+	url = 'https://google.com/search?q=Rotten%20Tomatoes%20' + formattedTitle + '%20' + '(' + titleYear + ')' + '%20';
+	//console.log(url)
+	
+	return url;
+}
+
+// ----------- SENDS CORRECT GOOGLE SEARCH URL TO BACKGROUND.JS FOR FETCH ----------------
+function search(url, RTspan, RTParentElem){
+	chrome.runtime.sendMessage(
+		url,
+			function (response) {
+				//console.log('correct info + hyperlinked returned');
+				append(response, RTspan, RTParentElem);
+			}
+		);
+}
+
+// ------- APPENDS CORRECT ROTTEN TOMATO INFO TO RTSPAN AND REMOVES LOADING.GIF ----------
+function append(RTinfo, RTspan, RTParentElem){
+	// Creates a new span, ...
+	var ratingSpan = document.createElement("span");
+	var textnode = document.createElement("p");
+	textnode.innerHTML = RTinfo + ' ';
+	ratingSpan.appendChild(textnode);
+	
+	// Creates a new image element, grabs url, and appends it to the RTspan
+	var popcornImage = document.createElement("img");
+	popcornImage.src = chrome.extension.getURL("images/popimage.png");
+
+	// Appends Popcorn image depending on if element is on single page / slider
+	if(RTParentElem.getElementsByClassName("Rotten-Tomatoes")[0]){
+		RTParentElem.getElementsByClassName("Rotten-Tomatoes")[0].appendChild(popcornImage); 
+	}
+	else{
+		RTParentElem.getElementsByClassName("Rotten-Tomatoes-SP")[0].appendChild(popcornImage); 
+	}
+	
+	// Styling for the entire RTspan class
+	RTspan.style.display = "flex";
+	
+	// Styling for the ratingSpan class
+	ratingSpan.setAttribute("style","padding-top:8px; padding-left:4px;");
+
+	// Finds loading image in RTspan
+	loading = RTspan.getElementsByClassName("loading")[0];
+	// .. and removes the loading image from the new RTspan
+	RTspan.removeChild(loading);
+	
+	// Appends data to the RTspan
+	if(RTParentElem.getElementsByClassName("Rotten-Tomatoes")[0]){
+		RTParentElem.getElementsByClassName("Rotten-Tomatoes")[0].appendChild(ratingSpan);
+	}
+	else{
+		RTParentElem.getElementsByClassName("Rotten-Tomatoes-SP")[0].appendChild(ratingSpan);
+	}
+	
+}
+// ====================== End of Function Definitions / Program Start =========================
+
+		//console.log (srcElement)
+
+		var x = slider(srcElement);             			     // Searches for slider element
+		var y = sliderOnSinglePage(srcElement);   				 // Searches for slider on single page
+		var z = singlePage();                     				 // Searches for single page 
+
+		if(x){        
+			//console.log("Moe - 1");                         
+			var RTspan = appendRottenTomatoes(x,rtClassName);    // Appends to slider element
+			url = sliderGoogleUrl(x);
+			//console.log(url);
+			search(url, RTspan, x);
+		}
+		else if(y){      
+			//console.log("Curly - 2");                          
+			var RTspan = appendRottenTomatoes(y,rtClassName);    // Appends to slider on single page
+			url = sliderGoogleUrl(y);
+			search(url, RTspan, y);
+		}
+		else if(z){      
+			//console.log("Larry - 3");                       
+			var RTspan = appendRottenTomatoes(z,rtClassNameSP);  // Appends to single page 
+			url = singlePageGoogleUrl();
+			search(url, RTspan, z);
+		}
+
+	}, false);
+});
